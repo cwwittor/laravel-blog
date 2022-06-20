34f27c9f19e25818df27f9e9e8c3380074d48445
Christian Wittorp <christian@swarminteractive.com>
2022-06-20 12:26:24 -0400

    Added Project to GitHub

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Post::factory()->create();
+
+        $user = User::factory()->create([
+            'name' => 'John Doe'
+        ]);
+
+        Post::factory(5)->create([
+            'user_id' => $user->id
+        ]);
+
+        // User::truncate();
+        // Post::truncate();
+        // Category::truncate();
+
+        // $user = User::factory()->create();
+
+        // $personal = Category::create([
+        //     'name' => 'Personal',
+        //     'slug' => 'personal'
+        // ]);
+
+        // $family = Category::create([
+        //     'name' => 'Family',
+        //     'slug' => 'family'
+        // ]);
+
+        // $work = Category::create([
+        //     'name' => 'Work',
+        //     'slug' => 'work'
+        // ]);
+
+        // Post::create([
+        //     'user_id' => $user->id,
+        //     'category_id' => $family->id,
+        //     'title' => 'My Family Post',
+        //     'slug' => 'my-first-post',
+        //     'excerpt' => '<p>Hiking is very enjoyable.</p>',
+        //     'body' => '<p>Hiking is very enjoyable. I went hiking in vermont a few summers ago.</p>'
+        // ]);
+
+        // Post::create([
+        //     'user_id' => $user->id,
+        //     'category_id' => $work->id,
+        //     'title' => 'My Work Post',
+        //     'slug' => 'my-second-post',
+        //     'excerpt' => '<p>Hiking is very enjoyable. Work Work Work</p>',
+        //     'body' => '<p>Hiking is very enjoyable. I went hiking in vermont a few summers ago. Work Work Work Work Work Work Work Work Work Work Work Work</p>'
+        // ]);
+    }
+}
