68b6fb7d6642da6b483b169117d82d0a2c4e068a
Christian Wittorp <christian@swarminteractive.com>
2022-06-22 16:15:06 -0400

    Add Login Page and Functionality

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,12 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
+
 class SessionsController extends Controller
 {
-  Public function destroy()
+
+  public function create()
+  {
+    return view('sessions.create');
+  }
+
+  public function store()
+  {
+    $attributes = request()->validate([
+      'email' => ['required', 'email'],
+      'password' => ['required']
+    ]);
+
+    if (auth()->attempt($attributes)) {
+      session()->regenerate();
+      return redirect('/')->with('success', 'Welcome Back!');
+    }
+
+    throw ValidationException::withMessages([
+      'email' => 'Your provided credentials could not be verified.'
+    ]);
+
+  }
+
+  public function destroy()
   {
     auth()->logout();
 
     return redirect('/')->with('success', 'Goodbye!');
   }
+
+
 }
